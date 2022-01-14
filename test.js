e656baf3fc95675187fb7b14f2a2b452ddfea60a
ngoctu.bui <buingoctu1998@gmail.com>
2022-01-14 12:04:06 +0700

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,7 @@
   kintone.events.on([
     'app.record.edit.change.work_date'
     ], function(event) {
+      console.log("testing");
       var new_date = moment(event.record.work_date.value).clone().startOf('month').format('YYYY-MM-DD');
       event.record.work_date.value = new_date;
       return event;
