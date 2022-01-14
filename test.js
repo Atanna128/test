451a899dc2db018f96159117cd249c8dc8bd046a
ngoctu.bui <buingoctu1998@gmail.com>
2022-01-14 12:02:59 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+  kintone.events.on([
+    'app.record.edit.change.work_date'
+    ], function(event) {
+      var new_date = moment(event.record.work_date.value).clone().startOf('month').format('YYYY-MM-DD');
+      event.record.work_date.value = new_date;
+      return event;
+  });
+})();
